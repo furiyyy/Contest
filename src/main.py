b4ac6c877b5706eea1d7b7c4f26ac5abb3e9fb3c
furiyyy <161677335+furiyyy@users.noreply.github.com>
2025-02-13 23:04:32 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
+from task1 import kwargsAcceptFun
+from task2 import typeBasedTransformer
+kwargsAcceptFun(name="Furi")
+
+
+result = typeBasedTransformer(
+    number=4,
+    text="Salom",
+    flag=True,
+    numbers_list=[1, 2, 3],
+    info={"a": 1, "b": 2}
+)
+
+print(result)
 
